bfa7debe37616c955a94fc71f80b99696114b3c0
Jacob McDonald <jacob@mcawesome.org>
2017-05-06 20:49:23 -0500

    Initial commit

---
+++ gcp_backup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#
+# This script runs gsutil rsync to synchronize several local datasets to Google
+# Cloud Storage for backup. The -m flag is used to parallelize the operation. If
+# the Internet connectivity is typical residential speeds of 10 Mbps upstream or
+# less, performance will likely suffer without removing the -m flag.
+#
+# Customization of the paths in this script are expected to be performed by the
+# user.
+#
+# Also available in a convenient Google Doc:
+# https://docs.google.com/document/d/1LSr3J6hdnCDQHfiH45K3HMvEqzbug7GeUeDa_6b_Hhc
+#
+# Revision 170422a-yottabit
+
+# Define the root directory of your backups here
+backupRoot="/config"
+
+# Define the Google Cloud Storage bucket name
+bucketName="gs://your-buckup-name"
+
+# Do some checks to ensure backup datasets are mounted
+[ ! -d "$backupRoot/Documents" ] && \
+echo "$backupRoot/Documents is missing, so exiting" && exit
+[ ! -d "$backupRoot/Music" ] && \
+echo "$backupRoot/Music is missing, so exiting" && exit
+[ ! -d "$backupRoot/Pictures" ] && \
+echo "$backupRoot/Pictures is missing, so exiting" && exit
+[ ! -d "$backupRoot/Video" ] && \
+echo "$backupRoot/Video is missing, so exiting" && exit
+
+echo Started `date`
+#dry-run with -n because mistakes cost money
+#time /usr/local/bin/gsutil -m rsync -rCdn "$backupRoot" "$bucketName"
+
+time /usr/local/bin/gsutil -m rsync -rCd "$backupRoot" "$bucketName"
+
+echo Finished `date`
---
+++ install-gsutil.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#
+# This script installs the Google Cloud SDK to enable the gsutil application. 
+# Next, a script (gcp_backup.sh) that performs a gsutil rsync operation to
+# backup local datasets to Google Storage is added to cron. Finally the script
+# is called the first time instead of waiting for cron. Should any part of the
+# script fail, hopefully it will just exit and leave the mess for the user to
+# correct.
+#
+# Also available in a convenient Google Doc:
+# https://docs.google.com/document/d/1LSr3J6hdnCDQHfiH45K3HMvEqzbug7GeUeDa_6b_Hhc
+#
+# Jacob McDonald
+# Revision 170420a-yottabit
+
+configDir=$(dialog --no-lines --stdout --inputbox "Persistent storage is:" 0 0 \
+"/config") || exit
+
+if [ -d "$configDir" ] ; then
+  echo "$configDir exists, like a boss!"
+else
+  echo "$configDir does not exist, so exiting (you might want to link a dataset)."
+  exit
+fi
+
+/usr/sbin/pkg update || exit
+/usr/sbin/pkg upgrade --yes || exit
+/usr/sbin/pkg install --yes google-cloud-sdk || exit
+/usr/sbin/pkg clean --yes || exit
+
+gcloud init || exit
+
+# add python path to crontab first
+sed -i '' -e 's~^PATH=.*~&:/usr/local/bin~' /etc/crontab || exit
+echo "42 02 * * * root \"$configDir/gcp_backup.sh\" \
+>> \"$configDir/gcp_backup.log\" 2>&1" >> /etc/crontab || exit
+
+"$configDir/gcp_backup.sh"
